2cee9ab05fb0381db06105922ab87b9899e831a8
oaxiom <oaxiom@gmail.com>
2022-03-30 20:21:50 +0800

    some DNBelab C4 read data

--- dnbelabc4/make_double_barcodes.py
+++ dnbelabc4/make_double_barcodes.py
@@ -1,23 +1,23 @@
 
 '''
 
-The C4 uses a double barcode system, I guess as it may help in barcode corection.
+The C4 uses a double barcode system, I guess as it may help in barcode correction.
 
-However, in practive barcode correction is around ~5% of total reads, and at best any
+However, in practice barcode correction is around ~5% of total reads, and at best any
 advantage from a Hamming distance on two barcodes is not much more than the Hamming distance from
 a single barcode. So merging the two barcodes to make a whitelist seems reasonable
 as it can now go through typical 10x-style analysis pipelines with no further modifications.
 
 '''
 
-oh = open('DNBelabC4barcodes_singles.txt', 'rt')
+oh = open('c4_barcodes.txt', 'rt')
 barcodes = []
 for lin in oh:
     barcodes.append(lin.strip())
 oh.close()
 
 done = 0
-oh = open('DNBelabC4barcodes_doubles.txt', 'wt')
+oh = open('c4_barcodes_doubles.txt', 'wt')
 for b1 in barcodes:
     for b2 in barcodes:
         oh.write(f"{b1}{b2}\n")
